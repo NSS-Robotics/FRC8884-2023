a0a5ba1413a7ed64f5c166d31604119176962c6f
Tom-the-Bomb <thello522@gmail.com>
2023-02-24 17:00:03 -0500

    formatting idk

--- src/main/java/frc/robot/commands/AlignLimeLight.java
+++ src/main/java/frc/robot/commands/AlignLimeLight.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
@@ -8,73 +7,59 @@ import frc.robot.subsystems.Limelight;
 import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import java.util.function.DoubleConsumer;
-import java.util.function.DoubleSupplier;
 import edu.wpi.first.math.geometry.Translation2d;
-
-
 import frc.robot.subsystems.Swerve;
 
 public class AlignLimeLight extends PIDCommand {
     private Swerve swerve;
     
-
-    public AlignLimeLight(PIDController controller, DoubleSupplier measurementSource, double setpoint,
-    DoubleConsumer useOutput, Subsystem[] requirements) {
-
+    public AlignLimeLight(
+        PIDController controller, DoubleSupplier measurementSource, double setpoint,
+        DoubleConsumer useOutput, Subsystem[] requirements
+    ) {
         super(controller, measurementSource, setpoint, useOutput, requirements);
-
     }
 
-    
-
     //TODO: Fix this constructor shit
-    
-    
     public AlignLimeLight(Swerve s_Swerve, Limelight limelight) {
         super(
-                new PIDController(
-                    Constants.turn_P, 
-                    Constants.turn_I, 
-                    Constants.turn_D
-                ),
-                limelight::gettx, 
-                0.0,
-                x -> s_Swerve.drive(
-                    new Translation2d(3, 0),
-                    0,
-                    false, false),
-                    s_Swerve
-                );
-    
-            
+            new PIDController(
+                Constants.turn_P, 
+                Constants.turn_I, 
+                Constants.turn_D
+            ),
+            limelight::gettx, 
+            0.0,
+            x -> s_Swerve.drive(
+                new Translation2d(3, 0),
+                0,
+                false, false
+            ),
+            s_Swerve
+        );
 
-    // Set the controller to be continuous (because it is an angle controller)
+        // Set the controller to be continuous (because it is an angle controller)
         getController().enableContinuousInput(-180, 180);
         // Set the controller tolerance - the delta tolerance ensures the robot is stationary at the
         // setpoint before it is considered as having reached the reference
         getController()
             .setTolerance(Constants.turnTolerance);
-            System.out.println("Align With Limelight - Start");
-            swerve = s_Swerve;
+            
+        System.out.println("Align With Limelight - Start");
+        swerve = s_Swerve;
+    } 
 
-
-        } 
-
-
-    
     @Override
     public boolean isFinished() {
-    // End when the controller is at the reference.
-      return getController().atSetpoint();
+        // End when the controller is at the reference.
+        return getController().atSetpoint();
     }
 
     @Override
     public void end(boolean interrupted) {
-      
         swerve.drive(new Translation2d(0,0), 0, false, false);
         System.out.println("Align With Limelight - End");
     }
 }
 
 /** A command that will turn the robot to the specified angle. */
-
