3df4c29f9d9de950d585a36649487220798d1b8d
Tom-the-Bomb <thello522@gmail.com>
2023-03-06 16:28:54 -0500

    fixed motor ids get

--- src/main/java/frc/robot/subsystems/BoomBox.java
+++ src/main/java/frc/robot/subsystems/BoomBox.java
@@ -4,16 +4,26 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.music.Orchestra;
 import java.util.ArrayList;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class BoomBox extends SubsystemBase {
   Orchestra orchestra;
 
   public BoomBox(String song) {
-    int[] ids = {4, 3, 6, 1, 2, 5, 7, 0};
-
+    int[] motors = {
+      Constants.Swerve.Mod0.driveMotorID,
+      Constants.Swerve.Mod0.angleMotorID,
+      Constants.Swerve.Mod1.driveMotorID,
+      Constants.Swerve.Mod1.angleMotorID,
+      Constants.Swerve.Mod2.driveMotorID,
+      Constants.Swerve.Mod2.angleMotorID,
+      Constants.Swerve.Mod3.driveMotorID,
+      Constants.Swerve.Mod3.angleMotorID,
+    };
+    
     ArrayList<TalonFX> instruments = new ArrayList<TalonFX>();
-    for (int id : ids) {
-      instruments.add(new TalonFX(id));
+    for (int motor : motors) {
+      instruments.add(new TalonFX(motor));
     }
 
     orchestra = new Orchestra(instruments);
