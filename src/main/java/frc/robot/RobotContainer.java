826bddfa35381180b00e3afc4e65455fddcd25de
Tom-the-Bomb <thello522@gmail.com>
2023-03-06 16:02:37 -0500

    fixed gh stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,7 +34,6 @@ public class RobotContainer {
   private final int rotationAxis = XboxController.Axis.kLeftX.value;
 
   /* Driver Buttons */
-<<<<<<< HEAD
   private final JoystickButton zeroGyro = new JoystickButton(
     driver,
     XboxController.Button.kY.value
@@ -64,21 +63,6 @@ public class RobotContainer {
     operator,
     XboxController.Button.kY.value
   );
-=======
-  private final JoystickButton zeroGyro =
-      new JoystickButton(driver, XboxController.Button.kY.value);
-  private final JoystickButton robotCentric =
-      new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-  private final JoystickButton rightBumper =
-      new JoystickButton(driver, XboxController.Button.kRightBumper.value);
-
-  private final JoystickButton bottomNode =
-      new JoystickButton(operator, XboxController.Button.kA.value);
-  private final JoystickButton midNode =
-      new JoystickButton(operator, XboxController.Button.kB.value);
-  private final JoystickButton topNode =
-      new JoystickButton(operator, XboxController.Button.kY.value);
->>>>>>> c50409ba5e9c105c8e0a383b8567b933ac7c0d55
 
   // private final JoystickButton up = new JoystickButton(
   //   operator,
@@ -95,12 +79,8 @@ public class RobotContainer {
   private final Elevator elevator = new Elevator();
   private final Claw claw = new Claw();
 
-<<<<<<< HEAD
   private final BoomBox boombox = new BoomBox("kv545.chrp");
   //private final Arm arm = new Arm();
-=======
-  // private final Arm arm = new Arm();
->>>>>>> c50409ba5e9c105c8e0a383b8567b933ac7c0d55
 
   // private final RunMotor runmotor = new RunMotor();
 
@@ -127,12 +107,8 @@ public class RobotContainer {
   private void configureButtonBindings() {
     /* Driver Buttons */
     zeroGyro.onTrue(new InstantCommand(s_Swerve::zeroGyro));
-<<<<<<< HEAD
     rightBumper.whileTrue(new AlignLimeLight(0, s_Swerve, limelight));
     music.whileTrue(new InstantCommand(boombox::play));
-=======
-    rightBumper.whileTrue(new AlignLimeLight(s_Swerve, limelight));
->>>>>>> c50409ba5e9c105c8e0a383b8567b933ac7c0d55
     // up
     //   .onTrue(new InstantCommand(runmotor::Extend))
     //   .onFalse(new InstantCommand(runmotor::Stop));
@@ -163,8 +139,8 @@ public class RobotContainer {
     midNode.whileTrue(new MidNode(elevator));
     topNode.whileTrue(new TopNode(elevator));
 
-    openClaw.whileTrue(new openClaw(claw));
-    closeClaw.whileTrue(new closeClaw(claw));
+    //openClaw.whileTrue(new openClaw(claw));
+    //closeClaw.whileTrue(new closeClaw(claw));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/BoomBox.java
+++ src/main/java/frc/robot/subsystems/BoomBox.java
@@ -6,19 +6,7 @@ import java.util.ArrayList;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BoomBox extends SubsystemBase {
-<<<<<<< HEAD
-    Orchestra orchestra;
-    
-    public BoomBox(String song) {
-        int[] ids = {4, 3, 6, 16, 2, 5, 7, 15};
-        
-        ArrayList<TalonFX> instruments = new ArrayList<TalonFX>();
-        for (int id : ids) {
-            instruments.add(new TalonFX(id));
-        }
-=======
   Orchestra orchestra;
->>>>>>> c50409ba5e9c105c8e0a383b8567b933ac7c0d55
 
   public BoomBox(String song) {
     int[] ids = {4, 3, 6, 1, 2, 5, 7, 0};
