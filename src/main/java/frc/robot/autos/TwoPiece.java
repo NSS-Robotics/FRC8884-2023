2d0316b29fc3f440ad09baa71245f287c05d192b
Tom-the-Bomb <thello522@gmail.com>
2023-03-27 08:48:55 -0400

    anoter

--- src/main/java/frc/robot/autos/TwoPiece.java
+++ src/main/java/frc/robot/autos/TwoPiece.java
@@ -5,7 +5,6 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
